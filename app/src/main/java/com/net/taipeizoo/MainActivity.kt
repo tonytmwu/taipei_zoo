a6c52a3db62e6a4b813ab120d97e8d45be81aa33
Tony <tonywu@taiwanmobile.com>
2020-11-27 18:27:58 +0800

    用CoordinatorLayout實作，ZooAreaDetailFragment圖片收合效果

--- app/src/main/java/com/net/taipeizoo/MainActivity.kt
+++ app/src/main/java/com/net/taipeizoo/MainActivity.kt
@@ -4,31 +4,57 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import com.google.gson.Gson
+import com.net.taipeizoo.databinding.ActivityMainBinding
+import com.net.taipeizoo.fragment.ZooAreaDetailFragment
 import com.net.taipeizoo.fragment.ZooAreaFragment
 import com.net.taipeizoo.fragment.ZooAreaFragmentDirections
 import com.net.taipeizoo.model.ZooArea
 
-class MainActivity : AppCompatActivity(), ZooAreaFragment.ZooAreaFragmentListener {
+class MainActivity : AppCompatActivity(),
+    ZooAreaFragment.ZooAreaFragmentListener,
+    ZooAreaDetailFragment.ZooAreaDetailFragmentListener {
 
+    private var _vb: ActivityMainBinding? = null
+    private val vb get() = _vb!!
     private val gson by lazy { Gson() }
     private lateinit var navController: NavController
 
     init {
         lifecycleScope.launchWhenStarted {
             navController = findNavController(R.id.fcvFragmentRoot)
+            setListener()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+    }
+
+    private fun setListener() {
+        navController.addOnDestinationChangedListener { _, _, _ ->
+            when(navController.previousBackStackEntry) {
+                null -> supportActionBar?.show()
+                else -> supportActionBar?.hide()
+            }
+        }
+    }
+
+    private fun popBackStack() {
+        navController.navigateUp()
     }
 
     override fun showDetail(data: ZooArea) {
         val json = gson.toJson(data)
         val direction = ZooAreaFragmentDirections.navToZooAreaDetailFragment(json)
         navController.navigate(direction)
+    }
+
+    override fun goBack() {
+        popBackStack()
     }
 }
--- app/src/main/java/com/net/taipeizoo/fragment/ZooAreaDetailFragment.kt
+++ app/src/main/java/com/net/taipeizoo/fragment/ZooAreaDetailFragment.kt
@@ -1,22 +1,34 @@
 package com.net.taipeizoo.fragment
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import coil.load
+import com.google.gson.Gson
 import com.net.taipeizoo.R
 import com.net.taipeizoo.databinding.FragmentZooAreaDetailBinding
+import com.net.taipeizoo.model.ZooArea
 
 class ZooAreaDetailFragment : Fragment() {
+
+    interface ZooAreaDetailFragmentListener {
+        fun goBack()
+    }
 
     private var _vb: FragmentZooAreaDetailBinding? = null
     private val vb get() = _vb!!
     private val vm = viewModels<ZooAreaDetailViewModel>()
     private val navArgs: ZooAreaDetailFragmentArgs by navArgs()
+    private val gson by lazy { Gson() }
+    private var zooArea: ZooArea? = null
+    private var listener: ZooAreaDetailFragmentListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +36,18 @@ class ZooAreaDetailFragment : Fragment() {
     ): View? {
         _vb = FragmentZooAreaDetailBinding.inflate(inflater, container, false)
         return vb.root
+    }
+
+    init {
+        lifecycleScope.launchWhenStarted {
+            processNavArgs()
+            setListener()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as? ZooAreaDetailFragmentListener
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -34,6 +58,22 @@ class ZooAreaDetailFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _vb = null
+    }
+
+    private fun processNavArgs() {
+        gson.fromJson(navArgs.zooArea, ZooArea::class.java)?.apply {
+            zooArea = this
+            vb.ivImg.load(imgUrl)
+            vb.toolbar.title = name
+            vb.tvInfo.text = info
+            vb.tvCategory.text = category
+        }
+    }
+
+    private fun setListener() {
+        vb.toolbar.setNavigationOnClickListener {
+            listener?.goBack()
+        }
     }
 
     companion object {
